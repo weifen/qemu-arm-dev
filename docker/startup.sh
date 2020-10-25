0d772860f53ae14c710c1e897ef4fcf052652860
giminose <fearlo1206@gmail.com>
2020-10-25 21:27:15 +0800

    run every thing inside the container
    1. copy qemu source into container.
    2. configure and make in the container.
    3. download raspbian image in the container.
    4. download raspi system kernel in the container.
    5. run qemu emulation with the startup.sh

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
 #MOUNTDIR#  # substituted during docker build
-mountdir=${mountdir:-'/qemu'}
+# mountdir=${mountdir:-'/qemu'}
 
 ## build QEMU in $mountdir
 
-cd ${mountdir}/source || exit 1
-mkdir -p bin/debug/native || exit 1
-cd bin/debug/native || exit 1
-../../../configure --enable-debug  --target-list=arm-softmmu,aarch64-softmmu || exit 1
-make -j $(nproc) || exit 1
-cd ../../.. || exit 1
+# cd ${mountdir}/source || exit 1
+# mkdir -p bin/debug/native || exit 1
+# cd bin/debug/native || exit 1
+# ../../../configure --enable-debug  --target-list=arm-softmmu,aarch64-softmmu || exit 1
+# make -j $(nproc) || exit 1
+# cd ../../.. || exit 1
 
-exec /bin/bash -l
+# exec /bin/bash -l
+image_path="/root/qemu-rpi-image/raspbian.img"
+emulator=/qemu/bin/debug/native/qemu-system-arm
+kernel="/root/qemu-rpi-kernel/kernel-qemu-4.19.50-buster"
+dtb="/root/qemu-rpi-kernel/versatile-pb.dtb"
+machine=versatilepb
+memory=256m
+root=/dev/sda2
+nic='--net nic --net user,hostfwd=tcp::5022-:22'
 
+echo "Booting QEMU machine \"${machine}\" with kernel=${kernel} dtb=${dtb}"
+exec ${emulator} \
+  --machine "${machine}" \
+  --cpu arm1176 \
+  --m "${memory}" \
+  --drive "format=raw,file=${image_path}" \
+  ${nic} \
+  --dtb "${dtb}" \
+  --kernel "${kernel}" \
+  --append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=${root} rootwait panic=1" \
+  --no-reboot \
+  --display none \
+  --serial mon:stdio
--- run.sh
+++ run.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-[ ! -d "./source" ] && git clone git://git.qemu-project.org/qemu.git && mv ./qemu ./source
-
-if [[ "$(docker images -q qemu-arm-dev 2> /dev/null)" == "" ]]; then
-  docker build -t qemu-arm-dev docker
-fi
-
+[ ! -d "./docker/source" ] && git clone git://git.qemu-project.org/qemu.git && mv ./qemu ./docker/source
+# if [[ "$(docker images -q qemu-raspi 2> /dev/null)" == "" ]]; then
+  docker build -t qemu-raspi docker
+# fi
 
 sleep 1
 
-echo 'To start a qemu-arm-dev container, run the following command.'
-echo 'docker run -it -v $PWD:/qemu qemu-arm-dev /bin/bash'
-
-
+echo 'To start a qemu-raspi container, run the following command.'
+echo 'docker run -it -v qemu-raspi /bin/bash'
