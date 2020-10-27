bd8ea456f229a006998e05e2e58217894bf63f5a
Wei-Fen Lin <weifen@skymizer.com>
2020-10-27 10:34:57 +0800

    update the bootup command

--- start-raspbian.sh
+++ start-raspbian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./bin/debug/native/qemu-system-arm  -M versatilepb  -cpu arm1176  -m 256  -hda ../system/2020-08-20-raspios-buster-armhf-lite.img  -net nic,  -net user,hostfwd=tcp::5022-:22  -dtb ../system/qemu-rpi-kernel/versatile-pb-buster-5.4.51.dtb  -kernel ../system/qemu-rpi-kernel/kernel-qemu-5.4.51-buster  -append 'root=/dev/sda2 panic=1'  -no-reboot  -nographic
+./bin/debug/native/qemu-system-arm  -M versatilepb  -cpu arm1176  -m 256  -drive "file=../system/2020-08-20-raspios-buster-armhf-lite.img,if=none,index=0,media=disk,format=raw,id=disk0" -device "virtio-blk-pci,drive=disk0,disable-modern=on,disable-legacy=off" -net nic,  -net user,hostfwd=tcp::5022-:22  -dtb ../system/qemu-rpi-kernel/versatile-pb-buster-5.4.51.dtb  -kernel ../system/qemu-rpi-kernel/kernel-qemu-5.4.51-buster  -append 'root=/dev/vda2 panic=1'  -no-reboot  -nographic
 
