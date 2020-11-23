2ed29ee8185c5d416fb2c98954c4094dff9c22f6
Wei-Fen Lin <weifen@skymizer.com>
2020-11-23 17:01:33 +0800

    fix docker name

--- run.sh
+++ run.sh
@@ -10,14 +10,14 @@
 [ ! -e "./system/Image" ] && wget http://releases.linaro.org/archive/15.06/openembedded/aarch64/Image 
 [ ! -e "./system/vexpress64-openembedded_lamp-armv8-gcc-4.9_20150620-722.img" ] && wget http://releases.linaro.org/archive/15.06/openembedded/aarch64/vexpress64-openembedded_lamp-armv8-gcc-4.9_20150620-722.img.gz && gzip -d ./system/vexpress64-openembedded_lamp-armv8-gcc-4.9_20150620-722.img.gz
  
-if [[ "$(docker images -q qemu-raspberry-pi 2> /dev/null)" == "" ]]; then
-  docker build -t qemu-raspberry-pi docker
+if [[ "$(docker images -q qemu-linaro-virt 2> /dev/null)" == "" ]]; then
+  docker build -t qemu-linaro-virt docker
 fi
 
 
 sleep 1
 
-echo 'To start a qemu-raspberry-pi container, run the following command.'
-echo 'docker run -it --privileged -v $PWD:/qemu qemu-raspberry-pi /bin/bash'
+echo 'To start a qemu-linaro-virt container, run the following command.'
+echo 'docker run -it --privileged -v $PWD:/qemu qemu-linaro-virt /bin/bash'
 
 
